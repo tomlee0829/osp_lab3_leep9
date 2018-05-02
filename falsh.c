aef5407e6d02eae3647317a82615883c81f979af
Lee <leep9@debian-9.3.0-template>
2018-05-02 16:10:52 -0700

    Shows current directory just like shell, lets user input by using keyboard(Commands doesn't do anything now)

--- falsh.c
+++ falsh.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#define BUFFERSIZE 100
 
 int main(int argc, char *argv[])
 {
@@ -13,7 +14,49 @@ int main(int argc, char *argv[])
 	char **commandName(const char *input); //writes back every command
 	int redirection(char *input); //Allowfor redirection between programs
 
-	return 0;
+	//Declare variables
+	char *path;
+       	char *buffer;
+	char *allCommand;
+	           
+	if((buffer=(char *)malloc(BUFFERSIZE*(sizeof(char))))==0)
+	{
+		printf("error! can't malloc enough space for buffer\n");					            
+		return 0;
+	}
+	
+	while(1)
+	{
+		path=getcwd(NULL,0);  //Get the current directory
+		printf("%s$ ", path);
+		allCommand=readCommand(buffer); //Read user inputs
+	}
+}
+
+//This function reads keyboard commands that
+//user enters, first step of the shell
+char *readCommand(char *buffer)
+{
+	char character;
+	char *userInput;
+	int inputLength = 0;
+
+	character = getchar();
+	while(character != '\n' && inputLength < BUFFERSIZE) //check input condition
+	{
+		buffer[inputLength] = character;
+		character = getchar();
+		inputLength++;
+	}
+
+	if(inputLength >= BUFFERSIZE) //Handlingh errors
+	{
+		fprintf(stderr, "Command is too long, please re enter!\n");
+		inputLength = 0;
+		return NULL;
+	}
+
+	return userInput;
 }
 
 //Built-in Commands
